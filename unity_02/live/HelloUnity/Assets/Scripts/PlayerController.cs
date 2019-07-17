76c61dce4b712b17a50f2377046eb38f1f193436
install <install@bps.local>
2019-07-17 14:15:05 +0200

    Added project from day 2

---
+++ unity_02/live/HelloUnity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+    public Camera MainCamera;
+
+    void Start()
+    {
+        
+    }
+
+    // REFACTOR
+    void PerformMovement()
+    {
+        float hm = Input.GetAxis("Horizontal");
+        Vector3 movementVector = new Vector3(hm, 0, 0);
+        Vector3 newPlayerPosW =
+            this.transform.position +
+            Speed * movementVector * Time.deltaTime;
+        Vector3 newPlayerPos =
+            MainCamera.WorldToScreenPoint(newPlayerPosW);
+
+        if (newPlayerPos.x >= 0 && newPlayerPos.x <= Screen.width)
+        {
+            this.transform.position = newPlayerPosW;
+        }
+    }
+
+    void PerformShooting()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("PEW PEW");
+        }
+    }
+
+    void Update()
+    {
+        PerformMovement();
+        PerformShooting();
+    }
+}
